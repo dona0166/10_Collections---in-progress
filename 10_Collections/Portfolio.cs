930b2bb9ae868baaa0aabb63607c3f49f9afbe8d
Donnie <Donnie@DESKTOP-A0TIJCR>
2016-09-28 21:53:15 +0200

    Add project files.

---
+++ 10_Collections/Portfolio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10_Collections
+{
+    public class Portfolio 
+    {
+        
+        public Portfolio()
+        {
+
+        }
+        public List<Asset> a = new List<Asset>();
+        public Portfolio(List<Asset> alist)
+        {
+           a = alist;
+        }
+        public double result;
+        public double GetTotalValue()
+        {
+            foreach(Asset element in a)
+            {
+                result += element.GetValue();
+            }
+            return result;
+        }
+
+        public void AddAsset(Asset anobject)
+        {
+
+            a.Add(anobject);
+
+        }
+
+        public IList<Asset> GetAssets()
+        {
+            
+        }
+    }
+}
